3a5bbdfd2bdb935448000b1961eddd123c3e5434
monocasa <tristan.hastings.miller@gmail.com>
2015-04-27 03:40:48 -0600

    Add stub source

---
+++ include/evpp/EventLoop.h
@@ -0,0 +1,13 @@
+#ifndef EVPP_EVENTLOOP_H
+#define EVPP_EVENTLOOP_H
+
+namespace evpp {
+
+class EventLoop
+{
+};
+
+} /*namespace evpp*/
+
+#endif /*EVPP_EVENTLOOP_H*/
+
---
+++ src/EventLoop.cpp
@@ -0,0 +1,2 @@
+#include "evpp/EventLoop.h"
+
